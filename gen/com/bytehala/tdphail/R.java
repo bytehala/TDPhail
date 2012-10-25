dd777843f25c431ca233fd42dfcbb592b9d9400b
bytehala <lemuel.dulfo@gmail.com>
2012-10-25 18:48:21 +0800

    a

--- gen/com/bytehala/tdphail/R.java
+++ gen/com/bytehala/tdphail/R.java
@@ -15,7 +15,7 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class string {
-        public static final int IgnoreTest=0x7f030001;
+        public static final int IgnoreTesta=0x7f030001;
         public static final int app_name=0x7f030000;
     }
     public static final class style {
